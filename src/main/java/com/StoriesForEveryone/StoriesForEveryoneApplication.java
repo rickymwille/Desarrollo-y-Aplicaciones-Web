e77b2bc3e7ed0f85e1b60d4089d82f9a0289cfab
rickymwille <91296178+rickymwille@users.noreply.github.com>
2022-06-05 20:11:33 -0600

    Primer Commit - se monta la base de las 8 paginas. Responsive activado

---
+++ src/main/java/com/StoriesForEveryone/StoriesForEveryoneApplication.java
@@ -0,0 +1,13 @@
+package com.StoriesForEveryone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoriesForEveryoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriesForEveryoneApplication.class, args);
+	}
+
+}
