ebcdfaada067a4376f1284ceafdf8c54353210f5
rickymwille <91296178+rickymwille@users.noreply.github.com>
2022-07-19 20:04:55 -0600

    Se agregan settings para el loggeado en la página.

---
+++ src/test/java/com/StoriesForEveryone/StoriesForEveryoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StoriesForEveryone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoriesForEveryoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
