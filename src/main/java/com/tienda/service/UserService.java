91804ebb725dafc23d0d0b03d5095e967c918f20
rickymwille <91296178+rickymwille@users.noreply.github.com>
2022-07-20 20:44:09 -0600

    Revert "Se añade el resto del login."
    This reverts commit e7376290e3c0c72c4e6ead8e4e8a465ea36f216d.

--- src/main/java/com/tienda/service/UserService.java
+++ src/main/java/com/tienda/service/UserService.java
@@ -9,13 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 /**
  *
  * @author ricky
  */
-@Service
 public class UserService implements UserDetailsService{
     
     @Autowired
