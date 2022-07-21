b47811716afb6305200766ed668d6cbeaf617017
rickymwille <91296178+rickymwille@users.noreply.github.com>
2022-07-20 20:43:12 -0600

    Revert "Modificaciones al login"
    This reverts commit 9244abf61a7b90dd13a31bbbf65235951ebcf4da, reversing
    changes made to e7376290e3c0c72c4e6ead8e4e8a465ea36f216d.

--- src/main/java/com/StoriesForEveryone/service/IBlogService.java
+++
@@ -1,9 +0,0 @@
-package com.StoriesForEveryone.service;
-
-import com.StoriesForEveryone.entity.Blog;
-import java.util.List;
-
-
-public interface IBlogService {
-    public List<Blog> listBlog();
-}
